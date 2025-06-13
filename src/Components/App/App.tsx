7b88aaffa23d630ba3726d68b003532768779381
Alex Savelev <savaqwerty2@gmail.com>
2025-06-13 23:15:10 +0200

    update hw

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -6,7 +6,7 @@ import css from "./App.module.css";
 import NoteList from "../NoteList/NoteList";
 import NoteModal from "../NoteModal/NoteModal";
 import Pagination from "../Pagination/Pagination";
-import SearchBox from "../../SearchBox/SearchBox";
+import SearchBox from "../SearchBox/SearchBox";
 import type { NotesResponse } from "../../types/note";
 import { fetchNotes } from "../../services/noteService";
 
