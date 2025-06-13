001eaf64ebf01cd05abefbaa853e404a3ecc64e9
Alex Savelev <savaqwerty2@gmail.com>
2025-06-13 23:27:34 +0200

    update hw

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import './index.css'
-import App from './Components/App/App'
+import App from './components-temp/App/App'
 
 const queryClient = new QueryClient();
 
